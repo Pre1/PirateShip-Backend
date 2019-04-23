402223b51f5b21c0c0f9549ba18d8abb7d2486d7
abdullah <anabanmi@hotmail.com>
2019-04-23 13:01:36 +0300

    init done

--- api/views.py
+++ api/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+    ListAPIView,
+    RetrieveAPIView,
+    RetrieveUpdateAPIView,
+    CreateAPIView,
+    DestroyAPIView,
+)
 
-# Create your views here.
+from rest_framework.views import APIView
+
+from django.http import Http404
+from rest_framework.response import Response
+from rest_framework import status
+
+from rest_framework.permissions import (
+    AllowAny,
+    IsAuthenticated,
+    IsAdminUser
+)
+
+from rest_framework.filters import (SearchFilter, OrderingFilter)
+
+## MODELS ##
+from django.contrib.auth.models import User
+
+
+
+# class UserCreateAPIView(CreateAPIView):
+#     serializer_class = UserCreateSerializer
